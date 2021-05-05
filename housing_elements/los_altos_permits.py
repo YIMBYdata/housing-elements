dfd051b25115817f837af33bf0c3c8233bcdbbc7
Salim <Salim@MacBook-Air.local>
2021-05-05 15:37:07 -0700

    rename cols in sf permits to match abag format. fix typo in los altos permits

--- housing_elements/los_altos_permits.py
+++ housing_elements/los_altos_permits.py
@@ -11,7 +11,7 @@ def load_all_permits(filter_post_2015_new_construction: bool = True, dedupe: boo
     permits = pd.read_csv('../data/clean_data/los_altos_clean.csv')
     permits = permits.drop(permits.columns[0], axis=1)
     permits.dropna(how='all', axis='columns', inplace=True)
-    date_cols = ['Issued', 'Finaled', 'Applied']
+    date_cols = ['Issued', ' Finaled', 'Applied']
     permits[date_cols] = permits[date_cols].apply(pd.to_datetime)
     renaming_map = {
         'APN': 'apn', 
--- housing_elements/san_francisco_permits.py
+++ housing_elements/san_francisco_permits.py
@@ -1,27 +1,39 @@
 import pandas as pd
 import geopandas as gpd
-
+import os
 
 def load_all_permits(filter_post_2015_new_construction: bool = True, dedupe: bool = True) -> pd.DataFrame:
-    sf_building_permits = pd.read_csv("../data/raw_data/sf_permits.csv")
-    date_cols = [c for c in sf_building_permits.columns if 'Date' in c]
-    sf_building_permits[date_cols] = sf_building_permits[date_cols].apply(pd.to_datetime)
-    sf_building_permits['apn'] = sf_building_permits['Block'] + '/' + sf_building_permits['Lot']
-    sf_building_permits['new_units'] = sf_building_permits['Proposed Units'].fillna(0) - sf_building_permits['Existing Units'].fillna(0)
+    path = os.path.join(os.path.dirname(__file__), "../data/raw_data/sf_permits.csv")
+    permits = pd.read_csv(path)
+    print(permits.columns)
+    date_cols = [c for c in permits.columns if 'Date' in c]
+    permits[date_cols] = permits[date_cols].apply(pd.to_datetime)
+    permits['apn'] = permits['Block'] + '/' + permits['Lot']
+    permits['new_units'] = permits['Proposed Units'].fillna(0) - permits['Existing Units'].fillna(0)
     relevant_uses = ['apartments', '1 family dwelling', '2 family dwelling', 
                  'residential hotel', 'misc group residns.', 'artist live/work', 
                  'convalescent home', 'accessory cottage', 'nursing home non amb',
                 'orphanage', 'r-3(dwg) nursing', 'nursing home gt 6']
-    sf_all_construction = sf_building_permits[
-        sf_building_permits['new_units'] > 0
-        & sf_building_permits['Proposed Use'].isin(relevant_uses)
-        & sf_building_permits['Permit Type'].isin([1, 2, 3, 8])
+    rhna_permits = permits[
+        permits['new_units'] > 0
+        & permits['Proposed Use'].isin(relevant_uses)
+        & permits['Permit Type'].isin([1, 2, 3, 8])
     ]
     if dedupe:
-        sf_all_construction.sort_values(by="Permit Type", axis=0, inplace=True)
-        sf_all_construction = sf_all_construction.drop_duplicates(['apn', 'new_units', 'Proposed Units', 'Street Number'])
+        rhna_permits.sort_values(by="Permit Type", axis=0, inplace=True)
+        rhna_permits = rhna_permits.drop_duplicates(['apn', 'new_units', 'Proposed Units', 'Street Number'])
     if filter_post_2015_new_construction:
-        sf_all_construction = sf_all_construction[
-            (sf_all_construction['Issued Date'] >= '2015-01-01')
+        rhna_permits = rhna_permits[
+            (rhna_permits['Issued Date'] >= '2015-01-01')
         ]
-    return sf_all_construction
+        
+    # Add / rename columns to fit ABAG format
+    rhna_permits['permyear'] = rhna_permits['Issued Date'].dt.year
+    rhna_permits = rhna_permits.rename({'new_units': 'totalunit'}, axis=1)
+    
+    # Address is split up into multiple columns. Must re-combine.
+    id_on_street = rhna_permits['Street Number'].astype(str) + " " + rhna_permits['Street Number Suffix'].fillna("")
+    street = rhna_permits['Street Name'] + ' ' + rhna_permits['Street Suffix']
+    rhna_permits['address'] = id_on_street + ' ' + street
+    
+    return rhna_permits
